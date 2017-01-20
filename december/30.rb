2861adec2077dcbbac55d263bfb429b4d39a3712
tylerstephens814 <tylerstephens814@gmail.com>
2017-01-20 11:38:06 -0700

    Complete ex 30

--- december/30.rb
+++ december/30.rb
@@ -1,4 +1,19 @@
+#Complete Jan 20, 2017
+
+
 require 'rspec'
+
+class Hash
+  def param_converter
+    pieces = []
+    string = ""
+    self.each do |a, b|
+      pieces << [a.to_s, b].join('=')
+      string = pieces.join('&')
+    end
+    string
+  end
+end
 
 describe 'HTML Param Converter' do
   it 'Adds an HTML param converter to the Hash class' do
@@ -6,4 +21,3 @@ describe 'HTML Param Converter' do
     expect(hash.param_converter).to eq('topic=baseball&team=astros')
   end
 end
-
