f1d8353b1b035c73bb19d55a5a980b925c29f299
tylerstephens814 <tylerstephens814@gmail.com>
2017-01-25 10:43:28 -0700

    Complete Ex. 15

--- january/15.rb
+++ january/15.rb
@@ -1,8 +1,17 @@
+#Complete Jan 25, 2017
+
 require 'rspec'
+
+class String
+  def alt_reverse
+    rev = ""
+    (self.length - 1).downto(0) {|x| rev << self[x]}
+    rev
+  end
+end
 
 describe 'Letter reversing' do
   it 'reverses the letters of a string without using the reverse method' do
     expect("Hi there".alt_reverse).to eq("ereht iH")
   end
 end
-
