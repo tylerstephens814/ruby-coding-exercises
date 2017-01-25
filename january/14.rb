2874d2eb8f68599787957f0ff8810dd450f2a232
tylerstephens814 <tylerstephens814@gmail.com>
2017-01-25 10:15:27 -0700

    Complete Ex. 14

--- january/14.rb
+++ january/14.rb
@@ -1,4 +1,12 @@
+#Complete Jan 25, 2017
+
 require 'rspec'
+
+class Array
+  def average
+    self.inject{|sum, n| sum + n}/self.size
+  end
+end
 
 describe 'Calculating Average' do
   it 'returns the average value from an array' do
