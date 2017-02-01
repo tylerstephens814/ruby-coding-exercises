417d163c3029da3af9587fd433614dbf3991d207
tylerstephens814 <tylerstephens814@gmail.com>
2017-02-01 09:40:51 -0700

    Complete Ex. 30

--- january/30.rb
+++ january/30.rb
@@ -1,10 +1,23 @@
+#Complete Feb 1, 2017
+
+
 require 'rspec'
 
 class ApiConnector
+  attr_accessor :attempts, :errors
+
+  def initialize
+    @attempts = 0
+    @errors = []
+  end
+
   def send_data
     begin
+      @attempts += 1
       api_call
     rescue Errno::ETIMEDOUT => e
+      @errors << e
+      retry if @attempts < 3
     end
   end
 
