e1d76e12ff57e773f08b103e312ffb10b723916d
tylerstephens814 <tylerstephens814@gmail.com>
2017-02-14 10:01:57 -0700

    Complete ex. 7

--- february/7.rb
+++ february/7.rb
@@ -1,4 +1,20 @@
+#Complete Feb 14, 2017 <3
+
 require 'rspec'
+
+# def cloned_cycle(arr, i)
+#   a = arr.to_a
+#   (i-1).times do
+#     arr.each do |e|
+#       a << e
+#     end
+#   end
+#   a
+# end
+
+def cloned_cycle(arr, i)
+  arr.cycle(i).to_a
+end
 
 describe 'Cloned cycle' do
   it 'Iterates through an array a variable number of times and returns the full set of elements' do
