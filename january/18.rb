cd83b25a42cf047e651e10a3fe9fceed24df307f
tylerstephens814 <tylerstephens814@gmail.com>
2017-02-15 10:45:24 -0700

    Solve ex. 18

--- january/18.rb
+++ january/18.rb
@@ -1,5 +1,18 @@
-#Apparently this is nothing.....thanks Jordan....
-
 def game
+  num = rand 25
+  puts "Guess a number between 0 and 24"
+
+  loop do
+    user_answer = gets.chomp.to_i
+    if user_answer == num
+      puts "Yay you got it right!"
+      break
+    elsif user_answer > num
+      puts "Guess lower."
+    elsif user_answer < num
+      puts "Guess higher."
+    end
+  end
 end
 
+game
